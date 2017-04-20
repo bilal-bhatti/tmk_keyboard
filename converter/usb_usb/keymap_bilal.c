083eec6025d0b21707f59285f32f42cf7a2c597e
Bilal Bhatti <bilal@cube.attlocal.net>
2017-04-19 22:05:39 -0500

    minor changes to the spacefn layout

--- converter/usb_usb/keymap_bilal.c
+++ converter/usb_usb/keymap_bilal.c
@@ -26,9 +26,9 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ESC, F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11, F12,           PSCR,SLCK,BRK,
     GRV, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSPC,     INS, HOME,PGUP,    TRNS,TRNS,TRNS,TRNS,
     TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSLS,     DEL, END, PGDN,    TRNS,TRNS,TRNS,
-    CAPS,A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,     ENT,                         TRNS,TRNS,TRNS,TRNS,
+    LCTL,A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,     ENT,                         TRNS,TRNS,TRNS,TRNS,
     LSFT,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,          RSFT,          UP,           TRNS,TRNS,TRNS,
-    LCTL,LALT,LGUI,          FN0,                     RGUI,RALT,APP, RCTL,     LEFT,DOWN,RGHT,    TRNS,TRNS,TRNS
+    CAPS,LALT,LGUI,          FN0,                     RGUI,RALT,FN0, RCTL,     LEFT,DOWN,RGHT,    TRNS,TRNS,TRNS
     ),
 
     /* 1: SpaceFN
@@ -36,23 +36,23 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |   |  |  |  |  |  |  |  |  |Prev|Play| |Next|Mute|Vold|Volu|
      * `---'  `-----------'  `---------------' `-------------------'
      * ,-----------------------------------------------------------.
-     * |   | F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12|Delete |
+     * |`  | F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12|Delete |
      * |-----------------------------------------------------------|
-     * |     |   |   |Esc|   |   |   |Hom|Up |End|Psc|Slk|Pau|Ins  |
+     * |    |   |   |VOUP|PGUP|   |   |Hom|End|   |Psc|Slk|Pau|Ins |
      * |-----------------------------------------------------------|
-     * |      |   |   |   |   |   |PgU|Lef|Dow|Rig|   |   |        |
+     * |      |   |   |VOLD|PGDN|   |Lef|Dow|Up |Rig|   |   |      |
      * |-----------------------------------------------------------|      ,----.
-     * |        |   |   |   |   |Spc|PgD|`  |~  |   |Men|          |      |Volu|
+     * |        |   |   |   |   |Spc|`  |~  |   |   |   |          |      |Volu|
      * |-----------------------------------------------------------| ,--------------.
      * |    |    |    |                        |    |    |    |    | |Mply|Vold|Mnxt|
      * `-----------------------------------------------------------' `--------------'
      */
     KEYMAP(
     TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,MPRV,MPLY,MNXT,MUTE,VOLD,VOLU,          PSCR,SLCK,BRK,
-    TRNS,F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11, F12, DEL,      INS, HOME,PGUP,    TRNS,TRNS,TRNS,TRNS,
-    TRNS,TRNS,TRNS,VOLU, TRNS,TRNS,TRNS,HOME,UP,  END, PSCR,SLCK,PAUS,INS,      DEL, END, PGDN,    TRNS,TRNS,TRNS,
-    TRNS,TRNS,MPLY,VOLD,MNXT,TRNS,PGUP,LEFT,DOWN,RGHT,TRNS,TRNS,     TRNS,                        TRNS,TRNS,TRNS,TRNS,
-    TRNS,TRNS,TRNS,TRNS,TRNS,SPC, PGDN,GRV, FN1, TRNS,APP,           TRNS,          VOLU,         TRNS,TRNS,TRNS,
+    GRV,F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11, F12, DEL,      INS, HOME,PGUP,    TRNS,TRNS,TRNS,TRNS,
+    TRNS,TRNS,TRNS,VOLU, PGUP,TRNS,TRNS,HOME,END,TRNS, PSCR,SLCK,PAUS,INS,      DEL, END, PGDN,    TRNS,TRNS,TRNS,
+    TRNS,TRNS,TRNS,VOLD, PGDN,TRNS,LEFT,DOWN,UP ,RGHT,TRNS,TRNS,     TRNS,                        TRNS,TRNS,TRNS,TRNS,
+    TRNS,TRNS,TRNS,TRNS, TRNS,SPC, GRV,FN1, TRNS, TRNS,TRNS,         TRNS,          VOLU,         TRNS,TRNS,TRNS,
     TRNS,TRNS,TRNS,          TRNS,                    TRNS,TRNS,TRNS,TRNS,     MPLY,VOLD,MNXT,    TRNS,TRNS,TRNS
     ),
 };
